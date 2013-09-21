8237d9f7eae4f68b1deb5512f0c48788a43c4815
tgreenfield@gmail.com <guapo0000>
2013-09-21 09:29:24 -0700

    Working around GA bug where WP8 OS version is not recorded. http://googleanalyticssdk.codeplex.com/workitem/40

--- GoogleAnalytics.Common/GAServiceManager.cs
+++ GoogleAnalytics.Common/GAServiceManager.cs
@@ -273,7 +273,7 @@ namespace GoogleAnalytics
             }
             else
             {
-                return string.Format("Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone {0}; Trident/6.0; IEMobile/10.0; ARM; Touch; {1}; {2})", Environment.OSVersion.Version, DeviceManufacturer, DeviceType);
+                return string.Format("Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone OS {0}; Trident/6.0; IEMobile/10.0; ARM; Touch; {1}; {2})", Environment.OSVersion.Version, DeviceManufacturer, DeviceType);
             }
         }
 
