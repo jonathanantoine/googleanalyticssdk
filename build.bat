8d3ec02c22579926a8919f567417ec077a611aae
timgreenfield <tgreenfield@gmail.com>
2014-04-20 18:06:38 -0700

    Including symbol fils in nuget package

--- build.bat
+++ build.bat
@@ -9,20 +9,30 @@
 "%DEVENV%" /build "Release|AnyCPU" GoogleAnalytics.VS2013.sln
 
 copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.Core.winmd" 	"deploy\lib\windows8\"
+copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.Core.pdb" 	"deploy\lib\windows8\"
 copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.winmd" 			"deploy\lib\windows8\"
+copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.pdb" 			"deploy\lib\windows8\"
 
 copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.Core.winmd" 	"deploy\lib\windows81\"
+copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.Core.pdb" 	"deploy\lib\windows81\"
 copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.winmd" 			"deploy\lib\windows81\"
+copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.pdb" 			"deploy\lib\windows81\"
 
 copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.Core.dll"    	"deploy\lib\windowsphone8\"
+copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.Core.pdb"    	"deploy\lib\windowsphone8\"
 copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.dll"    			"deploy\lib\windowsphone8\"
+copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.pdb"    			"deploy\lib\windowsphone8\"
 
 copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.Core.dll"    	"deploy\lib\wp71\"
+copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.Core.pdb"    	"deploy\lib\wp71\"
 copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.dll"    			"deploy\lib\wp71\"
+copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.pdb"    			"deploy\lib\wp71\"
 
 copy "Core\PCL.136\bin\Release\GoogleAnalytics.Core.dll"    					"deploy\lib\portable-windows8+net45\"
+copy "Core\PCL.136\bin\Release\GoogleAnalytics.Core.pdb"    					"deploy\lib\portable-windows8+net45\"
 
 copy "Core\PCL.104\bin\Release\GoogleAnalytics.Core.dll"    					"deploy\lib\portable-sl4+wp71+windows8\"
+copy "Core\PCL.104\bin\Release\GoogleAnalytics.Core.pdb"    					"deploy\lib\portable-sl4+wp71+windows8\"
 
 .nuget\nuget pack deploy\GoogleAnalyticsSDK.nuspec
 
