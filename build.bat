81b53ff1bc23a6fe09f574f002dd259ab7d47c91
tgreenfield@gmail.com <Guapo4599..>
2014-08-29 11:01:15 -0700

    Supporting adding reference to core within universal class library

--- build.bat
+++ build.bat
@@ -8,30 +8,33 @@
 @IF NOT EXIST "%DEVENV%" SET DEVENV=%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\VSWinExpress.exe
 "%DEVENV%" /build "Release|AnyCPU" GoogleAnalytics.VS2013.sln
 
-copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.Core.winmd" 	"deploy\lib\windows8\"
-copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.Core.pdb" 		"deploy\lib\windows8\"
-copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.winmd" 			"deploy\lib\windows8\"
-copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.pdb" 			"deploy\lib\windows8\"
-
-copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.Core.winmd" 	"deploy\lib\windows81\"
-copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.Core.pdb" 		"deploy\lib\windows81\"
+copy "Core\Universal\bin\Release\GoogleAnalytics.Core.winmd" 					"deploy\lib\windows81\"
+copy "Core\Universal\bin\Release\GoogleAnalytics.Core.pdb" 						"deploy\lib\windows81\"
 copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.winmd" 			"deploy\lib\windows81\"
 copy "GoogleAnalytics.Managed\Win81\bin\Release\GoogleAnalytics.pdb" 			"deploy\lib\windows81\"
 
-copy "GoogleAnalytics.Managed\WP81\bin\Release\GoogleAnalytics.Core.winmd" 		"deploy\lib\wpa81\"
-copy "GoogleAnalytics.Managed\WP81\bin\Release\GoogleAnalytics.Core.pdb" 		"deploy\lib\wpa81\"
+copy "Core\Universal\bin\Release\GoogleAnalytics.Core.winmd" 					"deploy\lib\wpa81\"
+copy "Core\Universal\bin\Release\GoogleAnalytics.Core.pdb" 						"deploy\lib\wpa81\"
 copy "GoogleAnalytics.Managed\WP81\bin\Release\GoogleAnalytics.winmd" 			"deploy\lib\wpa81\"
 copy "GoogleAnalytics.Managed\WP81\bin\Release\GoogleAnalytics.pdb" 			"deploy\lib\wpa81\"
 
-copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.Core.dll"    	"deploy\lib\windowsphone8\"
-copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.Core.pdb"    	"deploy\lib\windowsphone8\"
+copy "Core\WinRT.Win80\bin\Release\GoogleAnalytics.Core.winmd" 					"deploy\lib\windows8\"
+copy "Core\WinRT.Win80\bin\Release\GoogleAnalytics.Core.pdb" 					"deploy\lib\windows8\"
+copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.winmd" 			"deploy\lib\windows8\"
+copy "GoogleAnalytics.Managed\Win80\bin\Release\GoogleAnalytics.pdb" 			"deploy\lib\windows8\"
+
+copy "Core\SL.WP80\bin\Release\GoogleAnalytics.Core.dll"    					"deploy\lib\windowsphone8\"
+copy "Core\SL.WP80\bin\Release\GoogleAnalytics.Core.pdb"    					"deploy\lib\windowsphone8\"
 copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.dll"    			"deploy\lib\windowsphone8\"
 copy "GoogleAnalytics.Managed\WP80\bin\Release\GoogleAnalytics.pdb"    			"deploy\lib\windowsphone8\"
 
-copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.Core.dll"    	"deploy\lib\wp71\"
-copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.Core.pdb"    	"deploy\lib\wp71\"
+copy "Core\SL.WP71\bin\Release\GoogleAnalytics.Core.dll"    					"deploy\lib\wp71\"
+copy "Core\SL.WP71\bin\Release\GoogleAnalytics.Core.pdb"    					"deploy\lib\wp71\"
 copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.dll"    			"deploy\lib\wp71\"
 copy "GoogleAnalytics.Managed\WP71\bin\Release\GoogleAnalytics.pdb"    			"deploy\lib\wp71\"
+
+copy "Core\Universal\bin\Release\GoogleAnalytics.Core.winmd" 					"deploy\lib\portable-win81+wpa81\"
+@rem copy "Core\Universal\bin\Release\GoogleAnalytics.Core.pdb" 						"deploy\lib\portable-win81+wpa81\"
 
 copy "Core\PCL.136\bin\Release\GoogleAnalytics.Core.dll"    					"deploy\lib\portable-windows8+net45\"
 copy "Core\PCL.136\bin\Release\GoogleAnalytics.Core.pdb"    					"deploy\lib\portable-windows8+net45\"
