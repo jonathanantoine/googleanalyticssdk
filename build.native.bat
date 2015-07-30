91e0afb0c3d8e0b7b72e1fac91d71dc2d661907c
tgreenfield@gmail.com <Guapo4599..>
2015-07-30 09:10:28 -0700

    Updating vsix to support UWP/Win10 release

--- build.native.bat
+++ build.native.bat
@@ -1,12 +1,20 @@
 @pushd %~dp0%
 
+@set ZIP=%ProgramFiles%\7-Zip\7z.exe
+
 @set DEVENV=%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe
 @IF NOT EXIST "%DEVENV%" SET DEVENV=%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\VSWinExpress.exe
-@set ZIP=%ProgramFiles%\7-Zip\7z.exe
 
 "%DEVENV%" /build "Release|ARM" GoogleAnalytics.VS2013.sln
 "%DEVENV%" /build "Release|Win32" GoogleAnalytics.VS2013.sln
 "%DEVENV%" /build "Release|x64" GoogleAnalytics.VS2013.sln
+
+@set DEVENV=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe
+@IF NOT EXIST "%DEVENV%" SET DEVENV=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\VSWinExpress.exe
+
+"%DEVENV%" /build "Release|ARM" GoogleAnalytics.VS2015.sln
+"%DEVENV%" /build "Release|Win32" GoogleAnalytics.VS2015.sln
+"%DEVENV%" /build "Release|x64" GoogleAnalytics.VS2015.sln
 
 cd deploy.native
 cd win80
@@ -79,9 +87,32 @@ copy "..\..\Release\GoogleAnalytics.Native.WP81\GoogleAnalytics.Native.dll"		"Re
 "%ZIP%" a ..\GoogleAnalyticsSDK.WP81.zip "*"
 
 cd ..\
+cd win10
+
+rmdir /s /q "Redist\"
+rmdir /s /q "References\"
+
+mkdir "Redist"
+mkdir "Redist\CommonConfiguration"
+mkdir "Redist\CommonConfiguration\neutral"
+mkdir "Redist\CommonConfiguration\ARM"
+mkdir "Redist\CommonConfiguration\x86"
+mkdir "References"
+mkdir "References\CommonConfiguration"
+mkdir "References\CommonConfiguration\neutral"
+
+copy "..\..\Release\GoogleAnalytics.Native.Win10\GoogleAnalytics.winmd"				"References\CommonConfiguration\neutral\"
+copy "..\..\Release\GoogleAnalytics.Native.Win10\GoogleAnalytics.pri"				"Redist\CommonConfiguration\neutral\"
+copy "..\..\ARM\Release\GoogleAnalytics.Native.Win10\GoogleAnalytics.Native.dll"	"Redist\CommonConfiguration\ARM\"
+copy "..\..\Release\GoogleAnalytics.Native.Win10\GoogleAnalytics.Native.dll"		"Redist\CommonConfiguration\x86\"
+
+"%ZIP%" a ..\GoogleAnalyticsSDK.Win10.zip "*"
+
+cd ..\
 move GoogleAnalyticsSDK.Win80.zip Master\GoogleAnalyticsSDK.Win80.vsix
 move GoogleAnalyticsSDK.Win81.zip Master\GoogleAnalyticsSDK.Win81.vsix
 move GoogleAnalyticsSDK.WP81.zip Master\GoogleAnalyticsSDK.WP81.vsix
+move GoogleAnalyticsSDK.Win10.zip Master\GoogleAnalyticsSDK.Win10.vsix
 
 cd Master
 "%ZIP%" a ..\GoogleAnalyticsSDK.zip "*"
